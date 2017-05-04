62dd9c5cdfb3bfb9beb804f87bed5d03f7a9ff29
随风 <2272016819@qq.com>
2017-05-04 10:19:05 +0800

    trans time
    transfrom Date time

--- newDate/data.js
+++ newDate/data.js
@@ -17,3 +17,18 @@ time = Date.parse(time);   // 将当前时间转化为时间戳
 // 时间戳可进行加减后再转化为标准时间格式
 var time2 = new Date(time);
 // 之后可调用getLocalTime()方法将其转化成'2014-03-22 12:20:10'格式
+
+
+
+// 转化标准格式的时候由于ios不能识别除标准格式时间未的其他模式（例如2014-05-24 12:00）
+//故采用其他方法将其转化成标准格式然后再进行其他转化
+function stringToJsTime(timeString) {
+	var y = timeString.substring(0,4);
+    var m = timeString.substring(5,7)-1;
+    var d = timeString.substring(8,10);
+    var h = timeString.substring(11,13);
+    var mm = timeString.substring(14,16);      
+    var ss = timeString.substring(17,19);      
+    var time = new Date(y,m,d,h,mm,ss,0);      
+    return time;      
+}
